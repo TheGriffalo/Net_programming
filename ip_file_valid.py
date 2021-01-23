3721b85556dc8046ee5d9a7f0f0df776ef7c5e8c
TheGriffalo <paul@griffin.me.uk>
2021-01-23 20:08:13 +0000

    with nores

--- ip_file_valid.py
+++ ip_file_valid.py
@@ -13,8 +13,23 @@ def ip_file_valid():
 
     else:
         print("\n* File {} does not exist ;( Please check and try again.\n".format(ip_file))
+        sys.exit()
 
+    #Open user selected file for reading (IP addresses file)
+    selected_ip_file = open(ip_file, 'r')
 
+    #Starting from the beginning of the file
+    selected_ip_file.seek(0)
 
-ip_file_valid()
+    #Reading each line (IP address) in the file
+    ip_list = selected_ip_file.readlines()
+
+    #Closing the file
+    selected_ip_file.close()
+
+    return ip_list
+
+#Just to test standalone file
+ip_list = ip_file_valid()
+print(ip_list)
 
